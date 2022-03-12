f78f472600ba87cbe1f14bdbdebd0a74e536b445
Sher Chowdhury <sher.chowdhury@ibm.com>
2022-03-12 15:46:43 +0000

    wip

--- examples/03/test/unit/app-test.js
+++ examples/03/test/unit/app-test.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const request = require('supertest')
-const nock = require('nock')
 const expect = require('chai').expect
 
 describe('main tests', () => {
@@ -10,28 +9,15 @@ describe('main tests', () => {
     app = require('../../app').app
   })
 
-  const RUNTIME_ENDPOINT_URL = 'http://localhost'
-  let runtimeNock
-  beforeEach(() => {
-    runtimeNock = nock(RUNTIME_ENDPOINT_URL)
-  })
-
-  afterEach(() => {
-    nock.cleanAll()
-  })
-
   it('Successful request', async () => {
     const mockResponse = {
       "text": "hello",
     }
 
-    runtimeNock.get('/')
-      .reply(200, mockResponse)
-
     const res = await request(app)
       .get('/')
       .expect(200)
 
-    expect(res.text).to.equal('hello')
+    expect(res.text).to.equal('hello from my app')
   })
 })
