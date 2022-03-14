7f70cc333a862483a370b88402a02869aa725ba2
Sher Chowdhury <sher.chowdhury@ibm.com>
2022-03-14 14:14:59 +0000

    wip

--- examples/04/app.js
+++ examples/04/app.js
@@ -4,9 +4,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require('path') // this is a builtin package, so need to: npm install package
+const morgan = require('morgan')
 
 
-app.get(express.static(path.join(__dirname, "/public/")))
+app.use(morgan('combined')) // this is a debug feature that prints info on the cli everytime it receives a request. 
+
+const publicFolder = path.join(__dirname, '/public/')
+app.use(express.static(publicFolder))
 
 app.get('/', (req, res) => {
   res.send('hello from my app')
