df87511b7808928056c29d6a010ef6314823978c
Sher Chowdhury <sher.chowdhury@ibm.com>
2022-03-14 11:45:37 +0000

    wip

--- examples/03/server.js
+++ examples/03/server.js
@@ -6,7 +6,7 @@ const server = index.app.listen(index.port, () => {
   const port = server.address().port
   console.log(port)
 
-  let message = "Server started at: https://" + host + ":" + port
+  const message = 'Server started on port ' + port
 
   console.log(message)
 })
