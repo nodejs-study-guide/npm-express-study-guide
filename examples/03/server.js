0efa1e25acfa2164a98d08d653f49c9eb07aae40
Sher Chowdhury <sher.chowdhury@ibm.com>
2022-03-14 11:50:39 +0000

    wip

--- examples/03/server.js
+++ examples/03/server.js
@@ -4,9 +4,6 @@ const index = require('./app.js')
 
 const server = index.app.listen(index.port, () => {
   const port = server.address().port
-  console.log(port)
-
   const message = 'Server started on port ' + port
-
   console.log(message)
 })
