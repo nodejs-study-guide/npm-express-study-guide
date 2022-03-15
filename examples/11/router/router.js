3ff66b9c41738050c43b83689368760d8fec394d
Sher Chowdhury <sher.chowdhury@ibm.com>
2022-03-15 10:35:39 +0000

    wip

--- examples/11/router/router.js
+++ examples/11/router/router.js
@@ -5,9 +5,9 @@ const express = require('express')
 const router = express.Router()
 
 
-// We use colon to create a variable called userID. Then we can access it with req.params.userID
-router.get('/:name/:userID', (req, res) => {
-	res.send('my ' + req.params.name + ' is John and my id is: ' + req.params.userID)
+// We use colon to create a variable called userID. Then we can access it with req.params.userID`
+router.get('/name/:userID', (req, res) => {
+	res.send('my name is John and my id is: ' + req.params.userID)
 })
 
 module.exports = router
